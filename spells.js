f50d1903b40d6c80bd4ff774898131109ac3b9e5
Chris Mohen <cpmpal@gmail.com>
2019-01-27 12:40:57 -0500

    forgot that api does not 404 and need to read count result

--- spells.js
+++ spells.js
@@ -192,6 +192,7 @@ module.exports = {
         .then((parsedBody) => {
           // Succeeded
           console.log(parsedBody)
+          if(parsedBody['count'] === 0) reject("Explosive runes blind you. Make a race saving throw");
           resolve(parsedBody['results'][0]['url']);
         }, (failed) => reject("Explosive runes blind you. Make a race saving throw"))
         .catch((err) => {
